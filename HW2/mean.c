2a5aad68f34a1a4a4a5aceff324b4069b480427a
CooperNoam <cooper.noam@campus.technion.ac.il>
2023-04-22 16:16:05 +0000

    updated mean.c

--- HW2/mean.c
+++ HW2/mean.c
@@ -14,6 +14,10 @@ int main(int argc, char **argv){
     }
     else{
         f = fopen(argv[1], "r");
+        if(fclose(f) !=0 ){
+            fprintf(stderr, "Failed to close file\n");
+            return(1);
+        }
     }
 
     /* Check for errors */
@@ -27,6 +31,10 @@ int main(int argc, char **argv){
     /* Close file if its open */
     if (f != NULL) {
         fclose(f);
+        if(fclose(f) !=0 ){
+            fprintf(stderr, "Failed to close file\n");
+            return(1);
+        }
     }
 }
 
@@ -49,6 +57,7 @@ void operate( FILE *f){
         else if(retval != 1){
             /* Error */
             fprintf(stderr, "Error: not a number\n");
+            exit(1);
         }
         else if (grade_check(grade) == -1){
             /* Checks that grade is valid */
