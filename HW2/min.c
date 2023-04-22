506debeed9bac9753d082a15307ae3b1861435a7
CooperNoam <cooper.noam@campus.technion.ac.il>
2023-04-22 16:24:26 +0000

    updated min.c

--- HW2/min.c
+++ HW2/min.c
@@ -5,7 +5,6 @@
 void operate(FILE *f);
 int grade_check( int grade);
 
-
 int main(int argc, char **argv){
 
     FILE *f;
@@ -15,6 +14,10 @@ int main(int argc, char **argv){
     }
     else{
         f = fopen(argv[1], "r");
+        if(f == NULL ){
+            fprintf(stderr, "Failed to open file\n");
+            return(1);
+        }
     }
 
     /* Check for errors */
@@ -27,7 +30,10 @@ int main(int argc, char **argv){
 
     /* Close file if its open */
     if (f != NULL) {
-        fclose(f);
+        if(fclose(f) !=0 ){
+            fprintf(stderr, "Failed to close file\n");
+            return(1);
+        }
     }
 }
 
@@ -47,6 +53,8 @@ void operate( FILE *f){
         }
         else if(retval != 1){
             /* Error */
+            fprintf(stderr, "Error: not a number\n");
+            exit(1);
         }
         else if (grade_check(grade) == -1){
             /* Checks that grade is valid */
