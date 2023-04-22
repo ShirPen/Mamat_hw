ff0ee0ced93aa082565f6fc0b8c87249032edfb0
CooperNoam <cooper.noam@campus.technion.ac.il>
2023-04-22 15:38:04 +0000

    added calc_stats.sh

---
+++ HW2/calc_stats.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+	echo "Wrong number of arguments" 1>&2
+	exit 1
+fi
+
+course_num=$(basename "$1")
+
+if ! [[ -f "${course_num}.txt" ]]; then
+	echo "Course not found" 1>&2
+	exit 1
+fi
+#Checks that xxxxxx.txt exits
+
+if [[ -d "${course_num}_stats" ]]; then
+	rm -rf "${course_num}_stats"
+fi
+#Checks if a stats folder exists, and delets it and all its contents if it does
+
+mkdir "${course_num}_stats"
+
+for file in mean.exe median.exe min.exe max.exe; do
+
+	"./$file" < "${course_num}.txt" >> "statistics.txt"
+done
+#loop that runs each exe file
+#after running, it adds the result to the output file
+
+
+
+./hist -nbins 20 < "${course_num}.txt" > "pass.txt"
+
+pass=$(awk -F '\t' '$1 > 55 {sum += $2 } END {print sum} ' pass.txt)
+total=$(awk -F '\t' '{sum += $2} END {print sum}' pass.txt)
+pass_percent=$(expr $pass \* 100 / $total)
+#calculates the amount of students who passed and total valid grades
+
+#rm pass.txt 
+
+echo "$pass_percent%" >> "statistics.txt"
+
+./hist < "${course_num}.txt" > "histogram.txt"
+#Compiles and runs histogram 
+
+mv "statistics.txt" "${course_num}_stats"
+mv "histogram.txt" "${course_num}_stats"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
