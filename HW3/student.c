ab2778340d491d12ddf6f4c3d356245ccd155808
CooperNoam <cooper.noam@campus.technion.ac.il>
2023-05-07 17:59:42 +0000

    Added student.c

---
+++ HW3/student.c
@@ -0,0 +1,57 @@
+#include "student.h"
+#include <string.h>
+
+elem_t student_clone (elem_t e){
+
+    if(e == NULL){
+        return NULL;
+    }
+    else {
+
+        pstudent_t student_copy = malloc(sizeof(struct student));
+
+        if (student_copy == NULL) {
+            return NULL;
+        }
+
+        //Casts e into a student type
+        pstudent_t  temp_student = (struct student*)e;
+
+        student_copy->name = malloc(strlen(temp_student->name) + 1);
+        strcpy(student_copy->name, temp_student->name);
+
+
+        if(temp_student->age <= 0){
+            free(student_copy);
+            return NULL;
+        }
+
+        student_copy->age = temp_student->age;
+
+        if(temp_student->id < 0){
+            free(student_copy);
+            return NULL;
+        }
+
+        student_copy->id = temp_student->id;
+
+        return student_copy;
+
+    }
+}
+
+void student_destroy(elem_t e){
+
+    pstudent_t  student_dest = (struct student*)e;
+
+    free(student_dest->name);
+    free(student_dest);
+}
+
+void student_print(elem_t e){
+
+    pstudent_t  stud_p = (struct student*)e;
+
+    printf("student name: %s, age: %d, id: %d\n", stud_p->name, stud_p->age,
+                                                    stud_p->id);
+}
