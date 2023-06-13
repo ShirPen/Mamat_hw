8f54a257793197f22e6033f63caaf8337a26500b
Mamat <shir.pen@campus.technion.ac.il>
2023-06-13 16:57:28 +0300

    updated ip.cpp

--- HW6/ip.cpp
+++ HW6/ip.cpp
@@ -19,15 +19,12 @@ ip::ip(const String &ip) : GenericField() {
         mask_num = 0;
         mask_ip = 0;
 }
+// convert a string of number to int
 unsigned int int_convr(const String &ip){
 
     String *ip_bytes;
     size_t size = 0;
     ip.split(".", &ip_bytes, &size);
-
-//    if(size != TEST){
-//        return
-//    }//add check here
 
     unsigned int ip_int = 0;
     int curr;
@@ -46,10 +43,12 @@ bool ip::set_value(String value) {
     value.split("/", &ip_mask, &size);
     unsigned int int_ip = int_convr(ip_mask[0]);
     unsigned int mask = ip_mask[1].to_integer();
+    //checks that we got a valid mask number
     if(mask < 0 || mask > MASKMAX){
         delete[] ip_mask;
         return false;
     }
+
     this->mask_num = mask;
     this->mask_ip = int_ip >> (MASKMAX - mask_num);
     delete[] ip_mask;
@@ -60,19 +59,29 @@ bool ip::match(String packet) {
     String *packet_split;
     String *ip_addrs;
     size_t ps_size = 0; //size of packet split array
+    if(packet.equals("")){
+    	return false;
+    }
     packet.split(",",&packet_split,&ps_size);
     for(int i=0; i<(int)ps_size; i++){
         packet_split[i] = packet_split[i].trim();
         packet_split[i].split("=", &ip_addrs, &ps_size);
         ip_addrs[0] = ip_addrs[0].trim();
-        if(ip_addrs[0].equals(this->ip_name))
+        if(ip_addrs[0].equals(this->ip_name)){
             break;
+        }
+        delete[] ip_addrs;
     }
     //ip to be checked against mask
     unsigned int curr_ip = int_convr(ip_addrs[1]);
+    //remove the unwanted number of bits
     curr_ip = curr_ip >> (MASKMAX - mask_num);
+
+    //deallocate temporary memory allocations
     delete[] packet_split;
     delete[] ip_addrs;
+
+    //returns true if the ip number meets the rule
     return (curr_ip == this->mask_ip);
 }
 
